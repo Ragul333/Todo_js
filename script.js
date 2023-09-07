aa2ccfa0a23cdd2ffede75ba5c7645464d03f2ba
Ragul-S <ragul.s@kriyadocs.com>
2023-09-07 16:06:20 +0530

    Done using api

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 let input = document.getElementById("inputBox");
 let inputViaBtn = document.getElementById("button-addon");
 let itemsDiv = document.getElementById("items");
-let update = false;
-let updateId;
-let arr = [];
 
 input.addEventListener("keypress", (e) => {
   if (e.code === "Enter") {
-    arr.push(e.target.value);
+    createData(e.target.value);
     input.value = "";
-    render();
   }
 });
 
 inputViaBtn.addEventListener("click", (e) => {
-  arr.push(input.value);
+  createData(input.value);
   input.value = "";
-  render();
+  
 });
 
-function render() {
+function render(data) {
   itemsDiv.innerHTML = "";
-  arr.forEach((item, index) => {
+  data.forEach((item) => {
     let li = document.createElement("li");
-    li.innerText = item;
-    li.setAttribute("id", index);
+    li.innerText = item?.data;
+    li.setAttribute("id", item?.id);
 
     let deleteButton = document.createElement("button");
     deleteButton.innerText = "Delete";
     deleteButton.className = "btn btn-danger";
-    deleteButton.addEventListener("click", () => deleteButtonFn(index));
+    deleteButton.addEventListener("click", () => deleteButtonFn(item?.id));
 
     let updateButton = document.createElement("button");
     updateButton.innerText = "Update";
     updateButton.className = "btn btn-warning";
-    updateButton.addEventListener("click", () => updateButtonFn(index));
+    updateButton.addEventListener("click", () => updateButtonFn(item?.id,item?.data));
 
     let span = document.createElement("span");
     span.className = "groupBtn";
@@ -48,16 +44,82 @@ function render() {
   });
 }
 
-function deleteButtonFn(data) {
-  arr.splice(data, 1);
-  render();
+function deleteButtonFn(id) {
+  // arr.splice(data, 1);
+  deleteData(id);
 }
 
-function updateButtonFn(data) {
-  let upDatePrompt = prompt("Enter Item : ", arr[data]);
+function updateButtonFn(id,data) {
+  let upDatePrompt = prompt("Enter Item : ", data);
 
   if (upDatePrompt) {
-    arr[data] = upDatePrompt;
-    render();
+    upDateData(id,upDatePrompt)
   }
 }
+
+async function getDatas() {
+  const data = await fetch("https://6073db87066e7e0017e786f2.mockapi.io/data");
+  const jsonData = await data.json();
+  render(jsonData)
+}
+
+function createData(data) {
+  const newTask = {
+    data: data,
+  };
+
+  fetch("https://6073db87066e7e0017e786f2.mockapi.io/data", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(newTask),
+  })
+    .then((res) => {
+      if (res.ok) {
+        getDatas();
+        return res.json();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function upDateData(id,data) {
+  const newTask = {
+    data: data,
+  };
+
+  fetch(`https://6073db87066e7e0017e786f2.mockapi.io/data/${id}`, {
+    method: "PUT",
+    headers: { "content-type": "application/json" },
+    // Send your data in the request body as JSON
+    body: JSON.stringify(newTask),
+  })
+    .then((res) => {
+      if (res.ok) {
+        getDatas();
+        return res.json();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function deleteData(id) {
+
+  fetch(`https://6073db87066e7e0017e786f2.mockapi.io/data/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => {
+      getDatas();
+      console.log('Data deleted')
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+
+getDatas();
+
